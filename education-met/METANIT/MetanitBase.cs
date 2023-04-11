63dab911f0ce7a90387644871fd0ccfc3ad5d7b3
Ivan Gamanovich <igamanovich@elinext.com>
2023-04-12 00:15:18 +0300

    feat: add new Katas

--- education-met/METANIT/MetanitBase.cs
+++ education-met/METANIT/MetanitBase.cs
@@ -9,17 +9,7 @@ namespace education_met.METANIT
     internal class MetanitBase
     {
 
-        public static int[] SortNumbers(int[] nums)
-        {
-            if (nums is null) return new int[] {};
-            Array.Sort(nums, 0, nums.Length);
-            foreach (var item in nums)
-            {
-                Console.WriteLine(item);
-           
-            }
-            return nums;
-        }
+     
     }
 
 
