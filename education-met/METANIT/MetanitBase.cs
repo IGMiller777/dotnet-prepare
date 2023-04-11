dc8c90f1b512a7bcb235adcde7829bbb608e6678
Ivan Gamanovich <igamanovich@elinext.com>
2023-04-12 00:12:52 +0300

    feat: add new Katas

---
+++ education-met/METANIT/MetanitBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace education_met.METANIT
+{
+    internal class MetanitBase
+    {
+
+        public static int[] SortNumbers(int[] nums)
+        {
+            if (nums is null) return new int[] {};
+            Array.Sort(nums, 0, nums.Length);
+            foreach (var item in nums)
+            {
+                Console.WriteLine(item);
+           
+            }
+            return nums;
+        }
+    }
+
+
+  
+
+
+}
--- education-met/Program.cs
+++ education-met/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using education_met.KATA;
+using education_met.METANIT;
+
+KatasLibrary metanit = new KatasLibrary();
+Console.WriteLine(KatasLibrary.SortNumbers(new int[] { 1, 2, 10, 50, 5 }));
+Console.WriteLine(KatasLibrary.SortNumbers(new int[] {}));
